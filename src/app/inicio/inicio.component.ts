b8a5b21d24b80158c25cdc3f33e95ad8fa70b3c0
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2022-09-19 18:42:57 -0300

    versao1.2

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { Router } from '@angular/router';
+import { Postagem } from '../model/Postagem';
+
+import { Tema } from '../model/Tema';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +14,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  postagem: Postagem = new Postagem()
+  tema: Tema = new Tema()
+  user: User = new User()
 
-  ngOnInit(): void {
+  listaTemas: Tema[]
+  listaPostagem: Postagem[]
+
+  idTema: number
+
+  idUser = environment.id
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
+
+  ngOnInit(){
+
+    window.scroll(0,0)
+
+    if(environment.token == ''){
+       alert('Sua sessão expirou, faça o login novamente')
+      this.router.navigate(['/entrar'])
+    }
   }
-
 }
